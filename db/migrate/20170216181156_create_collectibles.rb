a67030b35a2c97d7e323768abb73c8d54da98b59
Nick Blaisdell <nick.blaisdell@gmail.com>
2017-02-21 14:30:03 -0500

    starting crud stories

--- db/migrate/20170216181156_create_collectibles.rb
+++ db/migrate/20170216181156_create_collectibles.rb
@@ -2,25 +2,25 @@ class CreateCollectibles < ActiveRecord::Migration[5.0]
   def change
     create_table :collectibles do |t|
       t.belongs_to :user, foreign_key: true
+
+      t.boolean :keep, default: :false
       t.integer :year
       t.string :media, null: false
-      t.string :title, null: false
+      t.string :name_of_item, null: false
+      t.string :story_title
+      t.integer :vol_num
       t.integer :issue_num
       t.string :publisher
-      t.string :writer
+      t.string :cover_artist
       t.string :artist
+      t.string :artist2
+      t.string :artist3
+      t.string :writer
       t.string :condition
-      t.decimal :value
-      t.text :desciption
+      t.decimal :est_value
+      t.text :notes
 
       t.timestamps null: false
     end
-
-    add_index :collectibles, :year
-    add_index :collectibles, :media
-    add_index :collectibles, :title
-    add_index :collectibles, :publisher
-    add_index :collectibles, :writer
-    add_index :collectibles, :artist
   end
 end
