47cc4c05533e1c2a0998aa7fe2e22aa0ec3ef29d
Nick Blaisdell <nick.blaisdell@gmail.com>
2017-04-10 16:19:48 -0400

    fixed some css

--- app/models/search.rb
+++ app/models/search.rb
@@ -11,7 +11,8 @@ class Search < ApplicationRecord
     collectibles = collectibles.where('name_of_item ILIKE :search OR
     story_title ILIKE :search OR writer ILIKE :search OR
     artist ILIKE :search OR artist2 ILIKE :search OR
-    artist3 ILIKE :search OR cover_artist ILIKE :search', search: "%#{keywords}%") if keywords.present?
+    artist3 ILIKE :search OR cover_artist ILIKE :search
+    OR notes ILIKE :search', search: "%#{keywords}%") if keywords.present?
     collectibles = collectibles.where(["publisher LIKE ?", publisher]) if publisher.present?
     collectibles = collectibles.where(["year = ?", year]) if year.present?
     collectibles = collectibles.where(["est_value >= ?", min_price]) if min_price.present?
